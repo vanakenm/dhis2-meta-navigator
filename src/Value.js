ad4ea792007e8fd950418518df0e12ed321c1f49
Martin Van Aken <martin@joyouscoding.com>
2018-01-01 18:00:20 +0100

    Add some icons

--- src/Value.js
+++ src/Value.js
@@ -2,37 +2,41 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { timeAgo } from "./lib/Utils";
 
-class Value extends Component {
-  isBasicType(value) {
-    return typeof value === "string" ||
-    typeof value === "number" ||
-    typeof value === "boolean";
-  }
+import ClearIcon from 'material-ui-icons/Clear';
+import DoneIcon from 'material-ui-icons/Done';
+import BlockIcon from 'material-ui-icons/Block';
 
+class Value extends Component {
   model(property) {
     return property.relativeApiEndpoint.slice(1,-1);
   }
 
   render() {
     const value = this.props.value;
-    if (value == null || this.props.property == null) {
-      return " - ";
+    const property = this.props.property;
+    
+    if (value == null || property == null) {
+      return <BlockIcon />;
     }
 
-    if (this.props.name === 'id' || (this.props.property.simple === true && this.props.property.propertyType !== 'ĈOLLECTION')) {
-      if (this.props.property.propertyType === 'DATE') {
+    if (this.props.name === 'id' || (property.simple === true && property.propertyType !== 'ĈOLLECTION')) {
+      if (property.propertyType === 'DATE') {
         return `${value} (${timeAgo(new Date(value))})`;
-      } else {
+      } else if (typeof value === "boolean") {
+        return value ? <DoneIcon /> : <ClearIcon />;
+      }
+      else {
         return value.toString();
       }
     } 
 
-    if(this.props.property.propertyType === 'REFERENCE') {
-      const model = this.model(this.props.property);
+    if(property.propertyType === 'REFERENCE') {
+      const model = this.model(property);
       return <span><Link to={`/collection/${model}/${value.id}`}>({value.id})</Link> ({model})</span>;
     } 
-    if(this.props.property.propertyType === 'COLLECTION' && this.props.property.itemPropertyType === 'REFERENCE') {
-      const model = this.model(this.props.property);
+    
+    if(property.propertyType === 'COLLECTION' && property.itemPropertyType === 'REFERENCE') {
+      const model = this.model(property);
       let values = [];
       value.forEach((v) => {
         values.push(<span key={v.id}><Link to={`/collection/${model}/${v.id}`}>{v.id}</Link> </span>);
@@ -41,6 +45,9 @@ class Value extends Component {
       return <div>{values} ({model})</div>;
     }
     else {
+      console.log("Not shown yet");
+      console.log(value);
+      console.log(property);
      return "";
     }
   }
