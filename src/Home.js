745d021a6edde6ab150a87a42bbb02970ebc7079
Martin Van Aken <martin@joyouscoding.com>
2018-04-24 22:54:34 +0200

    Home page

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,32 @@
-import React, { Component } from "react";
-import Paper from "material-ui/Paper";
-import PageTitle from "./components/PageTitle";
+import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+import PageTitle from './components/PageTitle';
+import Typography from 'material-ui/Typography';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <PageTitle>Welcome to DHIS2 Meta Navigator</PageTitle>
-        <Paper style={{ padding: "20px", margin: "20px" }}>
+        <Paper style={{ padding: '20px', margin: '20px' }}>
+          <p>
+            This app aim to make it easier to search, view and navigate through
+            DHIS2 so called "meta" - Data Elements, Org Units and so on.
+          </p>
+          <p>
+            It effectively make possible for a non admin user to check this
+            information without requiring knowledge of the API
+          </p>
           <p>Use the sidebar to start navigating.</p>
+
+          <b>Features</b>
+          <ul>
+            <li>List metas</li>
+            <li>Search on any field</li>
+            <li>Show any field</li>
+            <li>Navigate on click</li>
+            <li>Export lists as XLS</li>
+          </ul>
         </Paper>
       </div>
     );
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,36 +1,40 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Toolbar from 'material-ui/Toolbar';
 import AppBar from 'material-ui/AppBar';
 import Typography from 'material-ui/Typography';
 import IconButton from 'material-ui/IconButton';
-import Api from "./lib/Api";
+import Api from './lib/Api';
 
 const imageStyle = {
-    height: "20px"
+  height: '20px'
 };
 
 class NavigationBar extends Component {
-    render() {
-        return (
-          <AppBar position="static">
-            <Toolbar>
-                <IconButton color="contrast" aria-label="Menu">
-                <Link to={'/'}>
-                    <img
-                        src={`${Api.baseUrl}/api/26/staticContent/logo_banner`}
-                        style={imageStyle}
-                        alt="dhis2"
-                    />
-                </Link>
-                </IconButton>
-                <Typography title="DHIS2 Meta Navigator App" color="inherit" style={{margin: 'auto'}}>
-                    DHIS2 Meta Navigator App
-                </Typography>
-            </Toolbar>
-          </AppBar>
-        );
-    }
+  render() {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton color="contrast" aria-label="Menu">
+            <Link to="/">
+              <img
+                src={`${Api.baseUrl}/api/26/staticContent/logo_banner`}
+                style={imageStyle}
+                alt="dhis2"
+              />
+            </Link>
+          </IconButton>
+          <Typography
+            title="DHIS2 Meta Navigator App"
+            color="inherit"
+            style={{ margin: 'auto' }}
+          >
+            DHIS2 Meta Navigator App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
 export default NavigationBar;
